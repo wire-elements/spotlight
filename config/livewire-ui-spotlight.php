a3aaaf363e438934ccee9b8494856a0872384e80
Philo Hermans <me@philo.dev>
2021-10-28 21:14:29 +0200

    Update config and readme

--- config/livewire-ui-spotlight.php
+++ config/livewire-ui-spotlight.php
@@ -51,7 +51,7 @@ return [
     |
     | Spotlight will inject the required Javascript in your blade template.
     | If you want to bundle the required Javascript you can set this to false
-    | and add `require('vendor/livewire-ui/spotlight/resources/js/spotlight');`
+    | run `npm install --save fuse.js` and add `require('vendor/livewire-ui/spotlight/resources/js/spotlight');`
     | to your script bundler like webpack.
     |
     */
