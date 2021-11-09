eab471f67ec5df57ef0443d5710383ceca88bc41
Braunson Yager <braunson@gmail.com>
2021-11-09 17:44:16 -0500

    Reverted back spotlight view to support Alpine 2.x instead of 3.x  as it's a breaking change
    NOTE: When updating, you will need to update your published config to include the `include_js` key setting

--- resources/views/spotlight.blade.php
+++ resources/views/spotlight.blade.php
@@ -48,13 +48,13 @@
             </div>
             <div class="border-t border-gray-800" x-show="filteredItems().length > 0" style="display: none;">
                 <ul x-ref="results" style="max-height: 265px;" class="overflow-y-auto">
-                    <template x-for="(item, i) in filteredItems()" :key>
+                    <template x-for="([{ item: { id, name, description }}, i]) in filteredItems()" :key="id">
                         <li>
-                            <button @click="go(item[0].item.id)" class="block w-full px-6 py-3 text-left"
+                            <button @click="go(id)" class="block w-full text-left px-6 py-3"
                                     :class="{ 'bg-gray-700': selected === i, 'hover:bg-gray-800': selected !== i }">
-                                <span x-text="item[0].item.name"
+                                <span x-text="name"
                                       :class="{'text-gray-300': selected !== i, 'text-white': selected === i }"></span>
-                                <span x-text="item[0].item.description" class="ml-1"
+                                <span x-text="description" class="ml-1"
                                       :class="{'text-gray-500': selected !== i, 'text-gray-400': selected === i }"></span>
                             </button>
                         </li>
